a773202ebebc7d266412d3aea49eb45bedf0dd09
AndreFeh <andrefelipe.c.santos@gmail.com>
2022-11-22 22:21:53 -0300

    configurações iniciais --> Plugins, Swagger, Hello World, distribuicao de packages

---
+++ cadastro-usuarios/src/main/java/com/dio/cadastrousuarios/CadastroUsuariosApplication.java
@@ -0,0 +1,12 @@
+package com.dio.cadastrousuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroUsuariosApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroUsuariosApplication.class, args);
+		System.out.println("\n\t\t\t\t\t\t\t\t\t\t\t\t\t_________Aplicação Iniciada_________\t\t\t\t\t\t\t\t\t\t\t\t\t");
+	}
+}
